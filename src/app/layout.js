6874e71a28defef5e62014d8e88f854a845e8cab
mdyasin111 <mdyasin7636@gmail.com>
2023-08-07 16:26:21 +0600

    image explored

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
+import { Roboto } from "next/font/google";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "400", "500", "700"],
+  variable: "--font-roboto"
+});
 
 export const metadata = {
   title: "Next Hero",
@@ -11,7 +15,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} font-roboto`}>{children}</body>
     </html>
   );
 };
